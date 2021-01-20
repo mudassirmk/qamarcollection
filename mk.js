1c2ff1922367bd5b57dc4f48d0fb1f56c6240e2d
mudassirmk <mudassir.amazon.123@gmail.com>
2021-01-21 02:38:18 +0800

    mk

---
+++ mk.js
@@ -0,0 +1,35 @@
+
+const path=require("path");
+
+const express =require("express");
+const app=express();
+
+const port =3000;
+const staticpath = path.join(__dirname,"/public");
+
+//app.set("view engine","hbs");
+
+app.use(express.static(staticpath));
+app.get("/",(req,res)=>{
+
+ //   res.render("indexx");
+});
+
+app.get("/about",(req,res)=>{
+
+    res.end("ok");
+});
+
+app.get("/page",(req,res)=>{
+
+    res.end("ok");
+});
+
+app.get("/con",(req,res)=>{
+
+    res.end("ok");
+});
+
+app.listen(port,()=>{
+    console.log(`listening at port ${port}`);
+});
